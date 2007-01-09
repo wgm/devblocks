9887976ca9e024d2733f0d547a57023c8e3a6d35
Jeff Standen <jeff@webgroupmedia.com>
2007-01-09 03:56:28 +0000

    * Devblocks Platform
    git-svn-id: http://svn.webgroupmedia.com/devblocks/devblocks@4 747e5740-6425-0410-825b-f4fe3d30fc61

---
+++ smarty/plugins/block.devblocks_url.php
@@ -0,0 +1,17 @@
+<?php
+function smarty_block_devblocks_url($params, $content, &$smarty) {
+	$rewrite = true;
+
+	@$c = $params['c'];
+	@$a = $params['a'];
+
+	$url = URL::getInstance();
+	$contents = $url->write($c,$a,$content);
+	
+    if (!empty($params['assign'])) {
+        $smarty->assign($params['assign'], $contents);
+    } else {
+        return $contents;
+    }
+}
+?>
