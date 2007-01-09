f7c634b9bec5aa9021e2f36bf5ad901333150ac3
Jeff Standen <jeff@webgroupmedia.com>
2007-01-09 03:20:34 +0000

    * Cleared repository to make way for the Devblocks framework (as the project management app moved to DevBounty)
    git-svn-id: http://svn.webgroupmedia.com/devblocks/devblocks@3 747e5740-6425-0410-825b-f4fe3d30fc61

--- api/DevblocksModel.php
+++
@@ -1,9 +0,0 @@
-<?php
-class DbBounty {
-	public $id;
-	public $title;
-	public $estimate;
-	public $votes;
-}
-
-?>
--- dummy.php
+++
@@ -1,8 +0,0 @@
-<?php
-require(getcwd() . '/framework.config.php');
-require(CG_PATH . '/libs/cloudglue/CloudGlue.class.php');
-
-//// Agents
-//CerberusAgentDAO::createAgent('superuser','superuser',1);
-
-?>
