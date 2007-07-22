c3bcf9212a4dc9ace54088342ebb8b2813ddac09
Jeff Standen <jeff@webgroupmedia.com>
2007-07-22 10:25:37 +0000

    * Fixed an issue with different DB backends using UPPER CASE table names in MetaColumns().  The MySQL driver in ADODB will always be uppercase, while the PostgreSQL one won't.  We just stick with the defaults now and go upper.
    git-svn-id: http://svn.webgroupmedia.com/devblocks/devblocks@135 747e5740-6425-0410-825b-f4fe3d30fc61

--- patches/1.0.0_beta.php
+++ patches/1.0.0_beta.php
@@ -56,24 +56,24 @@ $prefix = (APP_DB_PREFIX != '') ? APP_DB_PREFIX.'_' : ''; // [TODO] Cleanup
 $tables = $datadict->MetaTables();
 
 // `plugin` ========================
-$columns = $datadict->MetaColumns($prefix.'plugin', false, false);
+$columns = $datadict->MetaColumns($prefix.'plugin');
 $indexes = $datadict->MetaIndexes($prefix.'plugin',false);
 
-if(!isset($columns['file'])) {
+if(!isset($columns['FILE'])) {
 	$sql = $datadict->AddColumnSQL($prefix.'plugin',"file C(128) DEFAULT '' NOTNULL");
 	$datadict->ExecuteSQLArray($sql);
 }
 
-if(!isset($columns['class'])) {
+if(!isset($columns['CLASS'])) {
 	$sql = $datadict->AddColumnSQL($prefix.'plugin',"class C(128) DEFAULT '' NOTNULL");
 	$datadict->ExecuteSQLArray($sql);
 }
 
 // `property_store` ========================
-$columns = $datadict->MetaColumns($prefix.'property_store', false, false);
+$columns = $datadict->MetaColumns($prefix.'property_store');
 $indexes = $datadict->MetaIndexes($prefix.'property_store',false);
 
-if(255 == @$columns['value']->max_length) {
+if(255 == @$columns['VALUE']->max_length) {
 	$datadict->ExecuteSQLArray($datadict->RenameColumnSQL($prefix.'property_store', 'value', 'value_old'));
 	$datadict->ExecuteSQLArray($datadict->AddColumnSQL($prefix.'property_store', "value B DEFAULT '' NOTNULL"));
 	
