58f50f55f1b76c2c39f41f5da5ac647768925de8
Jeff Standen <jeff@webgroupmedia.com>
2008-03-04 03:09:28 +0000

    git-svn-id: http://svn.webgroupmedia.com/devblocks/devblocks@208 747e5740-6425-0410-825b-f4fe3d30fc61

--- Devblocks.class.php
+++ Devblocks.class.php
@@ -6,7 +6,7 @@ include_once(DEVBLOCKS_PATH . "api/Extension.php");
 
 include_once(DEVBLOCKS_PATH . "libs/cloudglue/CloudGlue.php");
 
-define('PLATFORM_BUILD',206);
+define('PLATFORM_BUILD',208);
 
 /**
  *  @defgroup core Devblocks Framework Core
