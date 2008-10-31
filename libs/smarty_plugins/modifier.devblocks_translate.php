7a4ba2401e7e6656b12afb4db0fd901cc15175e7
Jeff Standen <jeff@webgroupmedia.com>
2008-10-31 04:30:26 +0000

    * [Translations] Removed the restrictions on the devblocks_translate Smarty modifier so it can use the full range of string tokens (%s, %d, arg reordering).  This will matter when languages need to change the order of arguments in strings.
    git-svn-id: http://svn.webgroupmedia.com/devblocks/devblocks@262 747e5740-6425-0410-825b-f4fe3d30fc61

--- libs/smarty_plugins/modifier.devblocks_translate.php
+++ libs/smarty_plugins/modifier.devblocks_translate.php
@@ -10,30 +10,7 @@ function smarty_modifier_devblocks_translate($string) {
 	array_shift($args); // pop off $string
 	
 	$translated = $translate->_($string);
-	
-	$hits = 0;
-	$pos = 0;
-	$find = '%s';
-	
-	if(extension_loaded('mbstring') && function_exists('mb_strpos')) {
-		while(false !== ($pos = mb_strpos($translated,$find,$pos,LANG_CHARSET_CODE))) {
-			$hits++;
-			$pos += mb_strlen($find); // skip the matched %s
-		}
-		
-	} else {
-		while(false !== ($pos = stripos($translated,$find,$pos))) {
-			$hits++;
-			$pos += strlen($find); // skip the matched %s
-		}
-		
-	}
-
-	// If we have enough arguments, go to town
-	if($hits == count($args))
-		$translated = @vsprintf($translated,$args);
-		
-	// Return what we can (sans replacements0
+	$translated = @vsprintf($translated,$args);
 	return $translated;
 }
 
