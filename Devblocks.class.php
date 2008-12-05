2ed8ce48c6047c5267082193d38c64c1d4833d9c
Jeff Standen <jeff@webgroupmedia.com>
2008-12-05 02:20:04 +0000

    * [Platform] Updated Smarty to version 2.6.21
    git-svn-id: http://svn.webgroupmedia.com/devblocks/devblocks@268 747e5740-6425-0410-825b-f4fe3d30fc61

--- Devblocks.class.php
+++ Devblocks.class.php
@@ -6,7 +6,7 @@ include_once(DEVBLOCKS_PATH . "api/Extension.php");
 
 //include_once(DEVBLOCKS_PATH . "libs/cloudglue/CloudGlue.php");
 
-define('PLATFORM_BUILD',266);
+define('PLATFORM_BUILD',267);
 
 /**
  *  @defgroup core Devblocks Framework Core
